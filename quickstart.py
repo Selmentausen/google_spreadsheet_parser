bc77a318588a4b282adbaf0aa496403541b85b47
Selmentausen <selmentausen@gmail.com>
2022-09-29 11:09:47 +0300

    Now tracks edit/add/deletion from spreadsheet A little refactoring

--- quickstart.py
+++ quickstart.py
@@ -12,10 +12,6 @@ SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/a
 conn = psycopg2.connect(database='test_case_db', user='postgres', password='admin123', host='127.0.0.1', port='5432')
 cursor = conn.cursor()
 
-credentials = service_account.Credentials.from_service_account_file('service_client_secret.json', scopes=SCOPES)
-client = gspread.authorize(credentials)
-sheet = client.open('test_data').sheet1
-
 
 def update_currency_file():
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
@@ -30,16 +26,50 @@ def get_usd_to_rub_exchange(xml_file):
     return float(item.text.replace(',', '.'))
 
 
-def update_db():
-    usd_to_rub_exchange = get_usd_to_rub_exchange('currency_exchange.xml')
-    for (p_id, order_id, usd_cost, delivery_date) in sheet.get("A2:D"):
-        rub_cost = float(usd_cost) * usd_to_rub_exchange
-        cursor.execute(
-            f"""INSERT INTO orders
-            VALUES ({p_id}, {order_id}, {usd_cost}, {rub_cost},
-            TO_DATE('{delivery_date}', 'DD.MM.YYYY'))""")
+def get_data_from_google_sheets(sheet):
+    return {int(order_id): {'usd_cost': int(usd_cost), 'delivery_date': delivery_date}
+            for _, order_id, usd_cost, delivery_date in sheet.get("A2:D")}
+
+
+def get_data_from_db():
+    cursor.execute("SELECT * FROM orders")
+    return {order_id: {'usd_cost': usd_cost, 'rub_cost': rub_cost, 'delivery_date': delivery_date}
+            for _, order_id, usd_cost, rub_cost, delivery_date in cursor.fetchall()}
+
+
+def convert_usd_to_rub(usd):
+    return float(usd) * usd_to_rub_exchange
+
+
+def update_db(sheet):
+    google_sheets_data = get_data_from_google_sheets(sheet)
+    db_data = get_data_from_db()
+    for order_id, values in google_sheets_data.items():
+        if order_id in db_data:
+            cursor.execute(f"""UPDATE orders 
+SET usd_cost={values['usd_cost']},
+rub_cost={convert_usd_to_rub(values['usd_cost'])}, 
+delivery_date=TO_DATE('{values['delivery_date']}', 'DD.MM.YYYY')
+WHERE order_id={order_id}""")
+            del db_data[order_id]
+        else:
+            cursor.execute(
+                f"""INSERT INTO orders (order_id, usd_cost, rub_cost, delivery_date)
+VALUES ({order_id}, {values['usd_cost']}, {convert_usd_to_rub(values['usd_cost'])},
+TO_DATE('{values['delivery_date']}', 'DD.MM.YYYY'))""")
+            print(f'added order {order_id}')
+    if db_data:
+        cursor.execute(f"""DELETE FROM orders WHERE order_id IN ({', '.join(map(str, db_data.keys()))})""")
+        print(f'deleted next orders {", ".join(map(str, db_data.keys()))}')
     conn.commit()
 
+
+# update_currency_file()
+credentials = service_account.Credentials.from_service_account_file('service_client_secret.json', scopes=SCOPES)
+client = gspread.authorize(credentials)
+data_sheet = client.open('test_data').sheet1
+usd_to_rub_exchange = get_usd_to_rub_exchange('currency_exchange.xml')
+update_db(data_sheet)
 
 cursor.execute("SELECT * FROM orders")
 data = cursor.fetchall()
