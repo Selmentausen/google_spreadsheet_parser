504d6a4f39b69e9194529abb6879963eb7725e2c
Selmentausen <selmentausen@gmail.com>
2022-09-29 11:51:55 +0300

    Added scheduling

--- quickstart.py
+++ quickstart.py
@@ -2,76 +2,91 @@ import gspread
 import psycopg2
 import xml.etree.ElementTree as ET
 import requests
+import schedule
+import time
 
 # noinspection PyPackageRequirements
 from google.oauth2 import service_account
 
-SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file",
-          "https://www.googleapis.com/auth/spreadsheets"]
 
-conn = psycopg2.connect(database='test_case_db', user='postgres', password='admin123', host='127.0.0.1', port='5432')
-cursor = conn.cursor()
-
-
-def update_currency_file():
+def update_currency_exchange():
+    global USD_TO_RUB_EXCHANGE_RATE
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    with open('currency_exchange.xml', 'wb') as f:
+    if not response:
+        return
+    with open(CURRENCY_EXCHANGE_FILE_PATH, 'wb') as f:
         f.write(response.content)
+    USD_TO_RUB_EXCHANGE_RATE = get_usd_to_rub_exchange()
 
 
-def get_usd_to_rub_exchange(xml_file):
-    tree = ET.parse(xml_file)
+def get_usd_to_rub_exchange():
+    tree = ET.parse(CURRENCY_EXCHANGE_FILE_PATH)
     root = tree.getroot()
     item = root.find("./Valute/[@ID='R01235']/Value")
     return float(item.text.replace(',', '.'))
 
 
 def get_data_from_google_sheets(sheet):
-    return {int(order_id): {'usd_cost': int(usd_cost), 'delivery_date': delivery_date}
-            for _, order_id, usd_cost, delivery_date in sheet.get("A2:D")}
+    return {int(item_id): {'order_id': int(order_id), 'usd_cost': int(usd_cost), 'delivery_date': delivery_date}
+            for item_id, order_id, usd_cost, delivery_date in sheet.get("A2:D")}
 
 
-def get_data_from_db():
+def get_data_from_db(cursor):
     cursor.execute("SELECT * FROM orders")
-    return {order_id: {'usd_cost': usd_cost, 'rub_cost': rub_cost, 'delivery_date': delivery_date}
-            for _, order_id, usd_cost, rub_cost, delivery_date in cursor.fetchall()}
+    return {item_id: {'order_id': order_id, 'usd_cost': usd_cost, 'rub_cost': rub_cost,
+                      'delivery_date': delivery_date.strftime('%d.%m.%Y')}
+            for item_id, order_id, usd_cost, rub_cost, delivery_date in cursor.fetchall()}
 
 
 def convert_usd_to_rub(usd):
-    return float(usd) * usd_to_rub_exchange
+    return float(usd) * USD_TO_RUB_EXCHANGE_RATE
 
 
-def update_db(sheet):
+def update_db():
+    conn = psycopg2.connect(database='test_case_db', user='postgres', password='admin123', host='127.0.0.1',
+                            port='5432')
+    cursor = conn.cursor()
+    sheet = client.open('test_data').sheet1
     google_sheets_data = get_data_from_google_sheets(sheet)
-    db_data = get_data_from_db()
-    for order_id, values in google_sheets_data.items():
-        if order_id in db_data:
-            cursor.execute(f"""UPDATE orders 
-SET usd_cost={values['usd_cost']},
+    db_data = get_data_from_db(cursor)
+    for item_id, values in google_sheets_data.items():
+        if item_id in db_data:
+            # google data is not subset of db data, then update data
+            if not set(google_sheets_data[item_id].values()) <= set(db_data[item_id].values()):
+                cursor.execute(f"""UPDATE orders 
+SET order_id={values['order_id']},
+usd_cost={values['usd_cost']},
 rub_cost={convert_usd_to_rub(values['usd_cost'])}, 
 delivery_date=TO_DATE('{values['delivery_date']}', 'DD.MM.YYYY')
-WHERE order_id={order_id}""")
-            del db_data[order_id]
+WHERE id={item_id}""")
+                print(f'changed order {values["order_id"]}')
+            del db_data[item_id]
         else:
             cursor.execute(
-                f"""INSERT INTO orders (order_id, usd_cost, rub_cost, delivery_date)
-VALUES ({order_id}, {values['usd_cost']}, {convert_usd_to_rub(values['usd_cost'])},
+                f"""INSERT INTO orders
+VALUES ({item_id}, {values['order_id']}, {values['usd_cost']}, {convert_usd_to_rub(values['usd_cost'])},
 TO_DATE('{values['delivery_date']}', 'DD.MM.YYYY'))""")
-            print(f'added order {order_id}')
+            print(f'added order {values["order_id"]}')
     if db_data:
-        cursor.execute(f"""DELETE FROM orders WHERE order_id IN ({', '.join(map(str, db_data.keys()))})""")
-        print(f'deleted next orders {", ".join(map(str, db_data.keys()))}')
+        cursor.execute(f"""DELETE FROM orders WHERE id IN ({', '.join(map(str, db_data.keys()))})""")
+        print(f'deleted next orders {", ".join(map(lambda v: str(v["order_id"]), db_data.values()))}')
     conn.commit()
+    conn.close()
+    print('database update finished!')
 
 
-# update_currency_file()
-credentials = service_account.Credentials.from_service_account_file('service_client_secret.json', scopes=SCOPES)
-client = gspread.authorize(credentials)
-data_sheet = client.open('test_data').sheet1
-usd_to_rub_exchange = get_usd_to_rub_exchange('currency_exchange.xml')
-update_db(data_sheet)
+if __name__ == '__main__':
+    SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file",
+              "https://www.googleapis.com/auth/spreadsheets"]
+    CURRENCY_EXCHANGE_FILE_PATH = 'currency_exchange.xml'
+    USD_TO_RUB_EXCHANGE_RATE = get_usd_to_rub_exchange()
+    credentials = service_account.Credentials.from_service_account_file('service_client_secret.json', scopes=SCOPES)
+    client = gspread.authorize(credentials)
 
-cursor.execute("SELECT * FROM orders")
-data = cursor.fetchall()
+    schedule.every().day.at('00:00').do(update_currency_exchange)
+    schedule.every(5).minutes.do(update_db)
 
-conn.close()
+    running = True
+    while running:
+        schedule.run_pending()
+        time.sleep(1)
